40cd7f76596ad6722fc6adfa2a9b26f0622b1a77
Moti Asayag <masayag@redhat.com>
2018-03-27 12:24:02 +0300

    Remove explicit vm creation

--- lib/fog/kubevirt.rb
+++ lib/fog/kubevirt.rb
@@ -1,1 +1,14 @@
-require 'fog/kubevirt/compute'
+require "fog/core"
+require "fog/xml"
+
+module Fog
+  module Compute
+    autoload :Kubevirt, File.expand_path('../kubevirt/compute', __FILE__)
+  end
+
+  module Kubevirt
+    extend Fog::Provider
+
+    service(:compute, "Compute")
+  end
+end
