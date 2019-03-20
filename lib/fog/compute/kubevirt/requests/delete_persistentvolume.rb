ca046597eb18aa80780b9366525e9af320312d24
Moti Asayag <masayag@redhat.com>
2019-03-20 17:20:44 +0200

    Rename Volume to PersistentVolume
    In fog, volume represents server's disk, which should be map to a
    representation of PVC and not a PV.
    
    Therefore, renaming the Volume to PersistentVolume, and in a latter PR
    will introduce a representation of Volume with desired attributes to be
    consumed by fog-kubevirt clients.

--- lib/fog/compute/kubevirt/requests/delete_persistentvolume.rb
+++ lib/fog/compute/kubevirt/requests/delete_persistentvolume.rb
@@ -2,13 +2,13 @@ module Fog
   module Compute
     class Kubevirt
       class Real
-        def delete_volume(name)
+        def delete_persistentvolume(name)
           kube_client.delete_persistent_volume(name)
         end
       end
 
       class Mock
-        def delete_volume(name)
+        def delete_persistentvolume(name)
         end
       end
     end
