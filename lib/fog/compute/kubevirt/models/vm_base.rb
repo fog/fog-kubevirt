c88696c0cca483a170ceb199cd581b9e81cc722c
Moti Asayag <masayag@redhat.com>
2019-03-24 22:54:06 +0200

    Replace VmVolume with Volume as main collection
    The 'volumes' collection will allow to list all volumes in the system
    that are backed by a PVC or to list all of vm volumes, if given a vm
    name while trying to list the volumes.
    
    The Volume class will be used also as a parameter for passing volume for
    a virtual machine create request, so additional information could be
    specified such as 'bus' and 'boot_order' to determine how the volume
    should be configured for the vm.
    
    With this PR also fixed the delete_vm service so cleanup after tests
    will work as expected when recording VCR for specs.

--- lib/fog/compute/kubevirt/models/vm_base.rb
+++ lib/fog/compute/kubevirt/models/vm_base.rb
@@ -33,14 +33,15 @@ module Fog
           annotations = metadata[:annotations]
           cpu = domain[:cpu]
           mem = domain.dig(:resources, :requests, :memory)
+          disks = parse_disks(domain[:devices][:disks])
           vm = {
             :namespace        => metadata[:namespace],
             :name             => metadata[:name],
             :resource_version => metadata[:resourceVersion],
             :uid              => metadata[:uid],
             :labels           => metadata[:labels],
-            :disks            => parse_disks(domain[:devices][:disks]),
-            :volumes          => parse_volumes(spec[:volumes]),
+            :disks            => disks,
+            :volumes          => parse_volumes(spec[:volumes], disks),
             :interfaces       => parse_interfaces(domain[:devices][:interfaces]),
             :networks         => parse_networks(spec[:networks]),
             :status           => object[:spec][:running].to_s == "true" ? "running" : "stopped",
