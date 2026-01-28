fog/fog-kubevirt

commit efbeed42ca086a9489a887a5f5967e15add054e8

Support SSL
Introduces two parameters for the kubevirt provider:
* kubevirt_verify_ssl - boolean 'true'/'false' indicates if SSL should be
used or not. Default is 'false'.
* kubevirt_cert_store - may contain either the trusted CA (pem format)
or a path to a file contains it.

lib/fog/kubevirt/compute/compute.rb
@@ -7,7 +7,8 @@ require "fog/core"
 module Fog
   module Kubevirt
     class Compute < Fog::Service
-      recognizes :kubevirt_token, :kubevirt_hostname, :kubevirt_port, :kubevirt_namespace, :kubevirt_log
+      recognizes :kubevirt_token, :kubevirt_hostname, :kubevirt_port, :kubevirt_namespace, \
+                 :kubevirt_log, :kubevirt_verify_ssl, :kubevirt_cert_store
 
       model_path 'fog/kubevirt/compute/models'
       model      :vminstance
@@ -191,6 +192,12 @@ module Fog
           @log ||= ::Logger.new(STDOUT)
 
           @namespace = options[:kubevirt_namespace] || 'default'
+          @opts = {
+            :ssl_options  => obtain_ssl_options(options),
+            :auth_options => {
+              :bearer_token => @kubevirt_token
+            }
+          }
 
           # Kubeclient needs different client objects for different API groups. We will keep in this hash the
           # client objects, indexed by API path/version.
@@ -465,6 +472,36 @@ module Fog
 
         def log
           @log
+        end
+
+        #
+        # Prepare the TLS and authentication options that will be used for the
+        # standard Kubernetes API and also for the KubeVirt extension
+        #
+        # @param options [Hash] a hash with connection options
+        #
+        def obtain_ssl_options(options)
+          if options[:kubevirt_verify_ssl] == true
+            ca = options[:kubevirt_cert_store] || ""
+            ca = IO.read(ca) if File.file?(ca)
+            certs = ca.split(/(?=-----BEGIN)/).reject(&:empty?).collect do |pem|
+              OpenSSL::X509::Certificate.new(pem)
+            end
+
+            cert_store = OpenSSL::X509::Store.new
+            certs.each do |cert|
+              cert_store.add_cert(cert)
+            end
+
+            ssl_options = {
+              :verify_ssl => OpenSSL::SSL::VERIFY_PEER,
+              :cert_store => cert_store
+            }
+          else
+            ssl_options = {
+              :verify_ssl => OpenSSL::SSL::VERIFY_NONE
+            }
+          end
         end
       end
 

commit 9603d79a239a0f68bedfc679cd1b65fbf6ec4753

Don't override `@opts` in `create_client_from_token`
`@opts` is already correctly set in `initialize` and if we override it,
we disable SSL validation

Fixes: efbeed42ca086a9489a887a5f5967e15add054e8
Fixes: CVE-2026-1530

lib/fog/kubevirt/compute/compute.rb
@@ -346,16 +346,6 @@ module Fog
         end
 
         def create_client_from_token(url)
-          # Prepare the TLS and authentication options that will be used for the standard Kubernetes API
-          # and also for the KubeVirt extension:
-          @opts = {
-            :ssl_options  => {
-              :verify_ssl => OpenSSL::SSL::VERIFY_NONE,
-            },
-            :auth_options => {
-              :bearer_token => @kubevirt_token
-            }
-          }
           version = detect_version(url.to_s, @opts[:ssl_options])
           key = url.path + '/' + version
 