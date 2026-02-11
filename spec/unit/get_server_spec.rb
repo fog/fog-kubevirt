eebbf32781597c69d427ce352e506a31556a842d
Arvind Jangir <ajangir@ajangir-thinkpadt14gen5.punetw6.csb>
2026-02-11 14:55:46 +0530

    Add spec for VMs created via foreman -> fog
    (cherry picked from commit 9b33704976771450e1b44e5364ec1e0e9d285841)

--- spec/unit/get_server_spec.rb
+++ spec/unit/get_server_spec.rb
@@ -31,4 +31,14 @@ describe Fog::Compute do
       assert_equal(server[:cpu_cores], 1)
     end
   end
+
+  it 'gets server created from fog-kubevirt' do
+    VCR.use_cassette('get_server_from_fog_kubevirt') do
+      server = @service.get_server('todd-vessell.foreman-isc.lan')
+
+      assert(server)
+      assert_equal(server[:memory], '1Gi')
+      assert_equal(server[:cpu_cores], 1)
+    end
+  end
 end
